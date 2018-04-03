ea031f87637bc406878cbd5f71c693b730f70408
Paul Dixon <paul@liblynx.com>
2018-04-03 12:53:49 +0100

    Simplified class phpdoc header

--- src/Sleep.php
+++ src/Sleep.php
@@ -5,7 +5,7 @@ namespace Elphin\LEClient;
 /**
  * In real world use, we want to sleep in between various actions. For testing, not so much.
  * So, we make it possible to inject a less sleepy service for testing
- * @package Elphin\LEClient
+ *
  * @codeCoverageIgnore
  */
 class Sleep
